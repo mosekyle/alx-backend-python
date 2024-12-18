f52d90ab801c99c8ea2746be7da1bf6cfdc9c7be
mosekyle <gitaumoseskiarie@gmail.com>
2024-12-18 14:52:10 +0300

    Added filters, status handling, and routing for Conversations and Messages API

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,32 +1,30 @@
 from rest_framework import viewsets
-from .models import User, Conversation, Message
-from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
-from rest_framework.permissions import IsAuthenticated
+from .models import User, Message, Conversation
+from .serializers import UserSerializer, MessageSerializer, ConversationSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
-# ViewSet for the User model (optional for this task, but useful for listing users)
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
-
-# ViewSet for the Conversation model
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    ordering_fields = '__all__'
+    filterset_fields = ['participants']
 
-    def perform_create(self, serializer):
-        # Optionally, you can customize how a conversation is created
-        # For example, you might want to set the current user as the creator or participant
-        serializer.save()
+    def create(self, request, *args, **kwargs):
+        # Custom logic for creating a conversation can be added here
+        return super().create(request, *args, **kwargs)
 
-# ViewSet for the Message model
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    ordering_fields = '__all__'
+    filterset_fields = ['conversation']
 
-    def perform_create(self, serializer):
-        # Optionally, customize how messages are created
-        # For example, you might want to automatically assign the sender to the current user
-        serializer.save(sender=self.request.user)
+    def create(self, request, *args, **kwargs):
+        # Custom logic for sending a message can be added here
+        return super().create(request, *args, **kwargs)
+
