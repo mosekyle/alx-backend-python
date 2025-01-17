60e3c877a730d446366c4edfb6150b7a2ac5050a
mosekyle <gitaumoseskiarie@gmail.com>
2025-01-17 15:59:54 +0300

    Add RequestLoggingMiddleware to log user requests

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
 
 ]
 
