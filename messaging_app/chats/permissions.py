dd87a0e365926212ecedf1a8b15f97a165c95914
mosekyle <gitaumoseskiarie@gmail.com>
2025-02-07 20:10:07 +0300

    Update changes

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -4,4 +4,9 @@ class IsParticipantOfConversation(BasePermission):
     def has_object_permission(self, request, view, obj):
         # Check if the user is a participant in the conversation
         return request.user in obj.participants.all()
+from rest_framework.permissions import BasePermission
+
+class IsParticipantOfConversation(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user in obj.participants.all()
 
